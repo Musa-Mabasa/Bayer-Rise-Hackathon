46fbe58a8263c42bd34f29e249225d0d967bfdc3
Farai Chivunga <u19068710@tuks.co.za>
2023-07-08 19:21:43 +0200

    ✨ Home Page and Progress indicator

--- zoeyapp/lib/authentication/login.dart
+++ zoeyapp/lib/authentication/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
-import 'package:zoeyapp/home.dart';
+import '../pages/home_page.dart';
 // import 'package:stem_wiz/src/authentication/phone_number_auth.dart';
 // import 'package:stem_wiz/src/home_page/home_page.dart';
 
@@ -106,7 +106,7 @@ class LoginPageState extends State<LoginPage> {
                         const Duration(milliseconds: 1500),
                         () {
                           Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => const Home()));
+                              builder: (context) => const HomePage()));
                           setState(() {
                             isValidating = false;
                           });
