fed8b89993f4cc9e04c0e8566a4f8b9a0bc76ee6
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2023-07-08 23:27:05 +0200

    Web code

---
+++ web/home.js
@@ -0,0 +1,13 @@
+
+
+function info() {
+    window.location.href = "infoCenter.html";
+}
+
+function health() {
+    window.location.href = "HealthCenter/index.html";
+}
+
+function ask() {
+    window.location.href = "AskMyPeers/index.html";
+}
