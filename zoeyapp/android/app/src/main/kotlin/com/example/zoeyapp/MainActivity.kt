e0aeb0a5275d89a64587f9039b715fbc0e34e755
Musa-Mabasa <musa14mabasa@gmail.com>
2023-07-08 13:31:27 +0200

    🚀 First flutter commit

---
+++ zoeyapp/android/app/src/main/kotlin/com/example/zoeyapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zoeyapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ zoeyapp/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:zoeyapp/authentication/login.dart';
+// import 'authentication/login.dart';
+// import 'lib/authentication/login.dart'
+// import "home.dart";
+
+void main() {
+  runApp(const MaterialApp(home: LoginPage()));
+}
